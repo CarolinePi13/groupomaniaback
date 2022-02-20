726f60fc643fc7c8b522c882ed51444defa51177
CarolinePi13 <coraline.mo63@gmail.com>
2022-02-20 14:49:03 +0100

    herokudb correction

--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,31 @@
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'postgres', process.env.DB_PASSWORD, {
-    host: 'ec2-54-74-60-70.eu-west-1.compute.amazonaws.com',
-    port:5432,
-    dialect: 'postgres'
-  });
+// const sequelize = new Sequelize('groupomania', 'postgres', process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     port:5432,
+//     dialect: 'postgres'
+//   });
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
 
-sequelize.authenticate().then(()=>{
+sequelize
+  .authenticate()
+  .then(() => {
     console.log('Connection has been established successfully.');
-    })
-    .catch ((error)=> {
-    console.error('Unable to connect to the database:', error);
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
+
+
 
   module.exports= sequelize;
